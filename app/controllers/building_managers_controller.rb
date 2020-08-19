7c1739452e5404c14199db2eb77dc8d27aab6071
melzreal <melissa.real@hotmail.com>
2020-08-19 12:17:40 +0100

    Remove scaffolded manager views

--- app/controllers/building_managers_controller.rb
+++ app/controllers/building_managers_controller.rb
@@ -1,10 +1,2 @@
 class BuildingManagersController < ApplicationController
-  def index
-    @building_managers = BuildingManager.all
-  end
-
-  private
-    def building_manager_params
-      params.require(:building_manager).permit(:email)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :building_managers
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "building_managers#index"
+  root to: "pages#index"
 end
