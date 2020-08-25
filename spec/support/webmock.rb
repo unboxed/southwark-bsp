8880a78dcc4db197c28d304a72a92552c23ad7aa
Pedro Moreira <pedro@pedrosmmoreira.com>
2020-08-25 10:36:12 +0100

    Correct webmock config typo
    We had a typo in the Webmock config - the key for allowing hosts is
    `allow` not `allowed`.
    
    This corrects the config to act properly

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,4 +1,4 @@
 require "webmock/rspec"
 
 allowed_hosts = ["chromedriver.storage.googleapis.com"]
-WebMock.disable_net_connect! allow_localhost: true, allowed: allowed_hosts
+WebMock.disable_net_connect! allow_localhost: true, allow: allowed_hosts
