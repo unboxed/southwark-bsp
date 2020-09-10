d4f1c84ffa79a128546b1df5ea291843dd6c0abf
Pedro Moreira <pedro@pedrosmmoreira.com>
2020-09-10 14:57:42 +0100

    Login to admin area
    In order to enter data and manage sending out surveys, an admin area was
    required. This should also be password protected and have no links
    accessible publicly.
    
    This PR adds an admin dashboard, secured by email/password, and
    accessible via "/admin" or "/admin/dashboard".
    
    In order to display navigation to the admin user, the application header
    was modified to include links, only displayable if a user is presently
    signed in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
-  has_secure_password
+  devise :database_authenticatable, :rememberable, :validatable
 end
