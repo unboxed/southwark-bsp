3b84c108264384a7a0d44e03f3935b82c973ad65
Andrew White <andrew.white@unboxed.co>
2021-03-26 09:29:17 +0000

    Restore old surveys#index at pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: "pages#index"
 
-  root to: "surveys#index"
   get "/help", to: "pages#help"
 
   devise_for :admin, only: :sessions, class_name: "User", module: "admin"
@@ -18,5 +18,4 @@ Rails.application.routes.draw do
   namespace :callbacks do
     resources :notification_statuses, only: [:create], constraints: lambda { |request| request.format == :json }
   end
-
 end
