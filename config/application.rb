8c99ff1f720b731cf3fa43d3afbbbee5a07d7250
Andrew White <andrew.white@unboxed.co>
2021-03-26 09:18:58 +0000

    Don't load all of the extra routes for Active Storage, etc.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +16,8 @@ Bundler.require(*Rails.groups)
 module SouthwarkBsp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
