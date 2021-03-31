3948f00fc52b1bb23833e94dbb87e0e397f1e170
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-03-31 10:18:49 +0100

    survey: map the data of each section

--- app/models/survey/record.rb
+++ app/models/survey/record.rb
@@ -6,17 +6,25 @@ module Survey
     store_accessor :data, :uprn
 
     # stage: ownership
-    store_accessor :data, :role, :right_to_manage_company
+    store_accessor :data, :role
     store_accessor :data, :full_name, :email, :organisation
     store_accessor :data, :ownership_details
 
     # stage: has residential
-    # TODO
+    store_accessor :data, :has_residential_usage # FIXME: this should be indicated by the next stage
 
     # stage: residential use
     store_accessor :data, :usage
 
-    # stage: status
-    store_accessor :data, :status, :status_details
+    # stage: management
+    store_accessor :data,
+                   :is_right_to_manage,
+                   :right_to_manage_company,
+                   :building_owner,
+                   :building_developer,
+                   :managing_agent
+
+    # stage: height
+    store_accessor :data, :height_in_metres, :number_of_storeys
   end
 end
--- app/models/survey/sections/building_management_form.rb
+++ app/models/survey/sections/building_management_form.rb
@@ -1,6 +1,19 @@
 module Survey
   module Sections
     class BuildingManagementForm < BaseForm
+      attribute :is_right_to_manage, :boolean
+
+      attribute :right_to_manage_company, :string
+      validates :right_to_manage_company, length: { maximum: 100 }
+
+      attribute :building_owner, :string
+      validates :building_owner, length: { maximum: 100 }
+
+      attribute :building_developer, :string
+      validates :building_developer, length: { maximum: 100 }
+
+      attribute :managing_agent, :string
+      validates :managing_agent, length: { maximum: 100 }
     end
   end
 end
