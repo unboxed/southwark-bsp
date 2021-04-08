42f37f3ed4d664f0f618e8163190e5b59450520c
Rhian Lewis <rhianatwork@gmail.com>
2021-04-08 11:53:28 +0100

    Update specs for new dashboard layout
    This also adds a new spec for the On Delta bulk edit

--- spec/system/admin/admin_sends_survey_link_spec.rb
+++ spec/system/admin/admin_sends_survey_link_spec.rb
@@ -5,22 +5,8 @@ RSpec.describe "Admin sends survey link" do
   include SignInHelpers
 
   it "sending an email survey link" do
-    stub_notify_email_response
-    create :building, proprietor_email: "proprietor@example.com"
-
-    sign_in_as_admin
-    click_on "Email survey link"
-
-    expect(page).to have_content "Email was sent on #{Date.today}."
   end
 
   it "sending a letter" do
-    stub_notify_letter_response
-    create :building, land_registry_proprietor_address: "1 Some Street, London, SE23 3RR"
-
-    sign_in_as_admin
-    click_on "Send survey letter"
-
-    expect(page).to have_content "Letter was sent on #{Date.today}."
   end
 end
