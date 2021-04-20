2f6781cd812b777aa7908d56d85ea728a30e70c2
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-04-20 14:38:12 +0100

    js: formatting

--- app/javascript/packs/bulk_actions.js
+++ app/javascript/packs/bulk_actions.js
@@ -1,18 +1,16 @@
 function checkAll() {
-    const boxes = document.querySelectorAll("#building_building_id_");
-    const cb = document.getElementById("building_id");
-    let i;
+  const boxes = document.querySelectorAll("#building_building_id_");
+  const cb = document.getElementById("building_id");
+  let i;
 
-    for (i = 0; i < boxes.length; i++) {
-        cb.checked ? boxes[i].checked = true : boxes[i].checked = false;
-    }
+  for (i = 0; i < boxes.length; i++) {
+    cb.checked ? (boxes[i].checked = true) : (boxes[i].checked = false);
+  }
 }
 
-document.addEventListener("DOMContentLoaded", (e) => {
-
-    document.getElementById("building_id").addEventListener("change", e => {
-        e.preventDefault();
-        checkAll();
-    })
-})
-
+document.addEventListener("DOMContentLoaded", e => {
+  document.getElementById("building_id").addEventListener("change", e => {
+    e.preventDefault();
+    checkAll();
+  });
+});
