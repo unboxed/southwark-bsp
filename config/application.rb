c233b951d99a46143eecb02a24d6a430d25472e1
Andrew White <andrew.white@unboxed.co>
2021-05-06 13:02:23 +0100

    Remove Action Cable, Sprockets, Active Storage
    None of these components are being used so lets save some memory.

--- config/application.rb
+++ config/application.rb
@@ -3,12 +3,10 @@ require_relative 'boot'
 require 'rails'
 
 require 'active_record/railtie'
-require 'action_cable/engine'
 require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'action_mailer/railtie'
 require 'active_job/railtie'
-require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
