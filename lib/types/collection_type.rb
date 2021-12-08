4546909fa054b1252193172d5a7c53f75b5237f7
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-12-08 16:22:50 +0000

    fix: linting

--- lib/types/collection_type.rb
+++ lib/types/collection_type.rb
@@ -3,7 +3,7 @@ require "ostruct"
 class CollectionType < ActiveModel::Type::Value
   attr_reader :collection_class, :item_class
 
-  def initialize(item: OpenStruct, collection: Array)
+  def initialize(item: Struct, collection: Array)
     @collection_class = collection
     @item_class = item
   end
