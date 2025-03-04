f0c62ca62555cd0ca15e826366f263992a204e44
Andrew White <andrew.white@unboxed.co>
2025-03-04 19:25:27 +0000

    Add user deactivation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   def first_name
     name.to_s.split(/\s+/).first
   end
+
+  def active_for_authentication?
+    super && !deactivated?
+  end
+
+  def inactive_message
+    deactivated? ? :deactivated : super
+  end
+
+  def deactivated?
+    deactivated_at && deactivated_at.past?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_04_171744) do
+ActiveRecord::Schema.define(version: 2025_03_04_172238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2025_03_04_171744) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.datetime "deactivated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
