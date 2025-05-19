caa444420c3cc6ee0342d1a17e4c62442bc0962e
Andrew White <andrew.white@unboxed.co>
2025-05-19 07:19:09 +0100

    Don't redefine methods after delegating

--- app/models/concerns/browseable.rb
+++ app/models/concerns/browseable.rb
@@ -119,7 +119,8 @@ module Browseable
     delegate :offset, :out_of_bounds?, to: :results
     delegate :next_page, :previous_page, to: :results
     delegate :total_entries, :total_pages, to: :results
-    delegate :each, :map, :to_a, to: :results
+    delegate :to_a, :to_ary, to: :results
+    delegate :each, :map, :size, to: :to_a
     delegate :count, to: :execute_search
 
     def initialize(klass, params = {})
@@ -128,10 +129,6 @@ module Browseable
 
     def current_page
       @current_page ||= [sanitized_page, 1].max
-    end
-
-    def each(&block)
-      results.each(&block)
     end
 
     def find_each(&block)
@@ -208,10 +205,6 @@ module Browseable
 
     def search?
       query.present?
-    end
-
-    def to_a
-      results.to_a
     end
 
     def in_batches(&block)
